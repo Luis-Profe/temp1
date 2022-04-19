559567782db9d7773baa33488e2b0fc511126b63
Luis-Profe <98078028+Luis-Profe@users.noreply.github.com>
2022-04-19 19:47:04 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
 basic.forever(function () {
-	
+    basic.showNumber(input.temperature())
+    basic.pause(2000)
+    basic.clearScreen()
+    if (input.temperature() >= 20) {
+        music.playMelody("A B A G A B C5 B ", 120)
+    }
 })
